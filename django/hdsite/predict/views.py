ba2b1f7e7d833e3abb5971b5b8e429d5d035e69e
nyinyinyanlin <nyinyinyanlin.mm@gmail.com>
2018-09-17 19:02:21 +0630

    Fixed Errors

--- django/hdsite/predict/views.py
+++ django/hdsite/predict/views.py
@@ -33,7 +33,6 @@ def post(request):
 			record = [];
 			for name in ["age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang","oldpeak","slope","ca","thal"]:
 				record.append(form.cleaned_data[name])
-			print(scaler.transform([record]))
 			record = scaler.transform([record])
 			return JsonResponse({'result':svc.predict(record)[0]})#svc.predict([record])[0]})
 	return JsonResponse({'result':-1})
